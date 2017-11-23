901cf022eab2ccc4b5910c3b2e1bcb4a86dc9dc2
Kingsley Osime <geekinthelead@gmail.com>
2017-11-23 01:55:43 +0000

    POJO resource

---
+++ src/main/java/kingsley/app/HelloResource.java
@@ -0,0 +1,18 @@
+package kingsley.app;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("greeting")
+public class HelloResource {
+	
+	@GET	
+	@Produces("text/html")
+	public String sayHello(@QueryParam("name") String name) {
+		
+		return "Hello" + name;
+	}
+
+}
